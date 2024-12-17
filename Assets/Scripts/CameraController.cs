c2d0db2ad18a7e35f2b171a84b32e674ada6a048
yuridomingues <yuridomingues.contato@gmail.com>
2024-12-17 13:59:09 -0300

    feat: adicionar projeto roll-a-ball unity

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+    
+}
